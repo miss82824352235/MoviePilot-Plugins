6ab61f5a13b4455d96d9c93599cbbdc09d59880e
GD <popeair85@gmail.com>
2026-07-05 05:15:38 +0800

    Update QBRawGuard v2.8.8 remove legacy cleanup risk paths

--- plugins.v2/qbrawguard/constants.py
+++ plugins.v2/qbrawguard/constants.py
@@ -10,7 +10,7 @@ QBRawGuard 常量与默认配置模块。
 from typing import Dict, Tuple
 
 
-PLUGIN_VERSION = "2.8.7"
+PLUGIN_VERSION = "2.8.8"
 
 TITLE_HINTS: Tuple[str, ...] = (
     "bdmv", "certificate", "video_ts", "audio_ts", "hvdvd_ts",
