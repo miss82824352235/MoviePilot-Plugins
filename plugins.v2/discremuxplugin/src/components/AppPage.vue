6a224e49d52bdaa18989e3f1a549b0df98c6ef87
GD <popeair85@gmail.com>
2026-07-16 06:10:35 +0800

    Update DiscRemuxPlugin 2.7.2

---
+++ plugins.v2/discremuxplugin/src/components/AppPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Page from './Page.vue'
+
+const props = defineProps({
+  api: { type: Object, required: true },
+})
+</script>
+
+<template>
+  <Page :api="props.api" />
+</template>
