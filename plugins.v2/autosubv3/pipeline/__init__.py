ab1a8164f20dfa7b769f668a1ac2be878af26587
GD <popeair85@gmail.com>
2026-07-17 16:43:56 +0800

    Publish AutoSubv3 v3.5.67-cleanup

---
+++ plugins.v2/autosubv3/pipeline/__init__.py
@@ -0,0 +1,7 @@
+from .asr_service import AsrService
+from .generation_pipeline import GenerationPipeline
+from .source_resolver import SourceResolver
+from .subtitle_output import SubtitleOutputService
+from .translation_service import TranslationService
+
+__all__ = ["AsrService", "GenerationPipeline", "SourceResolver", "SubtitleOutputService", "TranslationService"]
